c0b116b401a9f3662661e36593772d097441419c
Clergue Maël <mael.clergue@gmail.com>
2026-04-14 16:02:48 +0200

    maintenance

--- Assets/Mael/Modules/Lever/Codes/LeverCodes.cs
+++ Assets/Mael/Modules/Lever/Codes/LeverCodes.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "LeverCodes", menuName = "Scriptable Objects/LeverCodes")]
 public class LeverCodes : ScriptableObject
 {
     public int id;
     public int codeSize;
-    public int[] values;
+    public List<int> values;
 }
