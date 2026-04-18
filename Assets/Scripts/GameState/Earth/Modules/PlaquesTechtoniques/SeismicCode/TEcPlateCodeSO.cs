1563c07b269e9a10a7fa5a51d0bae7f9f3a9ef28
Clergue Maël <mael.clergue@gmail.com>
2026-04-18 22:51:55 +0200

    changesData

---
+++ Assets/Scripts/GameState/Earth/Modules/PlaquesTechtoniques/SeismicCode/TEcPlateCodeSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TEcPlateCodeSO", menuName = "Scriptable Objects/TEcPlateCodeSO")]
+public class TEcPlateCodeSO : ScriptableObject
+{
+    public int eventId;
+    public int[] options;
+}
